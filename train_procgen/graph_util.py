ca262e6c145d74ff3a9f789dc46ead34c9a9167c
Jurgis <jurgisp@gmail.com>
2020-12-30 14:39:11 +0200

    Tweaking graph_util

--- train_procgen/graph_util.py
+++ train_procgen/graph_util.py
@@ -5,9 +5,10 @@ import matplotlib
 from math import ceil
 from constants import ENV_NAMES
 
-import seaborn # sets some style parameters automatically
+import seaborn  # sets some style parameters automatically
 
 COLORS = [(57, 106, 177), (218, 124, 48)]
+
 
 def switch_to_outer_plot(fig):
     ax0 = fig.add_subplot(111, frame_on=False)
@@ -15,6 +16,7 @@ def switch_to_outer_plot(fig):
     ax0.set_yticks([])
 
     return ax0
+
 
 def ema(data_in, smoothing=0):
     data_out = np.zeros_like(data_in)
@@ -31,25 +33,28 @@ def ema(data_in, smoothing=0):
 
     return data_out
 
+
 def plot_data_mean_std(ax, data_y, color_idx=0, data_x=None, x_scale=1, smoothing=0, first_valid=0, label=None):
     color = COLORS[color_idx]
     hexcolor = '#%02x%02x%02x' % color
 
-    data_y = data_y[:,first_valid:]
+    data_y = data_y[:, first_valid:]
     nx, num_datapoint = np.shape(data_y)
 
     if smoothing > 0:
         for i in range(nx):
-            data_y[i,...] = ema(data_y[i,...], smoothing)
+            data_y[i, ...] = ema(data_y[i, ...], smoothing)
 
     if data_x is None:
         data_x = (np.array(range(num_datapoint)) + first_valid) * x_scale
 
     data_mean = np.mean(data_y, axis=0)
-    data_std = np.std(data_y, axis=0, ddof=1)
-
     ax.plot(data_x, data_mean, color=hexcolor, label=label, linestyle='solid', alpha=1, rasterized=True)
-    ax.fill_between(data_x, data_mean - data_std, data_mean + data_std, color=hexcolor, alpha=.25, linewidth=0.0, rasterized=True)
+
+    if data_y.shape[0] > 1:
+        data_std = np.std(data_y, axis=0, ddof=1)
+        ax.fill_between(data_x, data_mean - data_std, data_mean + data_std, color=hexcolor, alpha=.25, linewidth=0.0, rasterized=True)
+
 
 def read_csv(filename, key_name):
     with open(filename) as csv_file:
@@ -68,23 +73,36 @@ def read_csv(filename, key_name):
 
     return np.array(values, dtype=np.float32)
 
-def plot_values(ax, all_values, title=None, max_x=0, label=None, **kwargs):
+
+def plot_values(ax, all_values, title=None, max_x=0, label=None, data_x=None, **kwargs):
     if max_x > 0:
-        all_values = all_values[...,:max_x]
+        all_values = all_values[..., :max_x]
+        if data_x is not None:
+            data_x = data_x[:max_x]
 
     if ax is not None:
-        plot_data_mean_std(ax, all_values, label=label, **kwargs)
+        plot_data_mean_std(ax, all_values, label=label, data_x=data_x, **kwargs)
         ax.set_title(title)
 
     return all_values
 
-def plot_experiment(run_directory_prefix, titles=None, suffixes=[''], normalization_ranges=None, key_name='eprewmean', **kwargs):
-    run_folders = [f'{run_directory_prefix}{x}' for x in range(3)]
 
-    num_envs = len(ENV_NAMES)
-    will_normalize_and_reduce = normalization_ranges is not None
+def plot_experiment(run_directory_prefix,
+                    titles=None,
+                    suffixes=[''],
+                    key_name='eprewmean',
+                    x_key_name=None,
+                    env_names=None,
+                    num_runs=3,
+                    will_reduce=False,
+                    normalization_ranges=None,
+                    **kwargs):
+    run_folders = [f'{run_directory_prefix}{x}' for x in range(num_runs)]
 
-    if will_normalize_and_reduce:
+    env_names = env_names or ENV_NAMES
+    num_envs = len(env_names)
+
+    if will_reduce:
         num_visible_plots = 1
         f, axarr = plt.subplots()
     else:
@@ -94,12 +112,11 @@ def plot_experiment(run_directory_prefix, titles=None, suffixes=[''], normalizat
 
     for suffix_idx, suffix in enumerate(suffixes):
         all_values = []
-        game_weights = [1] * num_envs
 
         for env_idx in range(num_envs):
-            env_name = ENV_NAMES[env_idx]
-            label = suffix if env_idx == 0 else None # only label the first graph to avoid legend duplicates
-            print(f'loading results from {env_name}...')
+            env_name = env_names[env_idx]
+            label = suffix if env_idx == 0 else None  # only label the first graph to avoid legend duplicates
+            # print(f'loading results from {env_name}...')
 
             if num_visible_plots == 1:
                 ax = axarr
@@ -108,22 +125,27 @@ def plot_experiment(run_directory_prefix, titles=None, suffixes=[''], normalizat
                 ax = axarr[env_idx // dimy][env_idx % dimy]
 
             csv_files = [f"results/{resid}/progress-{env_name}{'-' if len(suffix) > 0 else ''}{suffix}.csv" for resid in run_folders]
-            curr_ax = None if will_normalize_and_reduce else ax
+            curr_ax = None if will_reduce else ax
 
             raw_data = np.array([read_csv(file, key_name) for file in csv_files])
-            values = plot_values(curr_ax, raw_data, title=env_name, color_idx=suffix_idx, label=label, **kwargs)
 
-            if will_normalize_and_reduce:
+            raw_data_x = None
+            if x_key_name:
+                raw_data_x = read_csv(csv_files[0], x_key_name)
+
+            if normalization_ranges is not None:
                 game_range = normalization_ranges[env_name]
                 game_min = game_range[0]
                 game_max = game_range[1]
-                game_delta = game_max - game_min
-                sub_values = game_weights[env_idx] * (np.array(values) - game_min) / (game_delta)
-                all_values.append(sub_values)
+                raw_data = (np.array(raw_data) - game_min) / (game_max - game_min)
 
-        if will_normalize_and_reduce:
-            normalized_data = np.sum(all_values, axis=0)
-            normalized_data = normalized_data / np.sum(game_weights)
+            values = plot_values(curr_ax, raw_data, title=env_name, color_idx=suffix_idx, label=label, data_x=raw_data_x, **kwargs)
+            all_values.append(values)
+
+            
+
+        if will_reduce:
+            normalized_data = np.mean(all_values, axis=0)
             title = 'Mean Normalized Score'
             plot_values(ax, normalized_data, title=None, color_idx=suffix_idx, label=suffix, **kwargs)
 
